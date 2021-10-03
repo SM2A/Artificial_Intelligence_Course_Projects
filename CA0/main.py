1264d7ab380a57fe50a848580fe933b9513c4e2d
SM2A <seyedamin1990@gmail.com>
2021-10-03 23:23:44 +0330

    Part 3

--- CA0/main.py
+++ CA0/main.py
@@ -2,6 +2,9 @@ import pandas
 
 initial_data = pandas.read_csv('CA0\\FuelConsumptionCo2.csv')
 
-# print(initial_data.head)
-# print(initial_data.tail)
-print(initial_data.info())
+initial_data['FUELTYPE'] = initial_data['FUELTYPE'].astype('category').cat.codes
+initial_data['ENGINESIZE'].fillna(value=initial_data['ENGINESIZE'].mean(),inplace=True)
+initial_data['CYLINDERS'].fillna(value=initial_data['CYLINDERS'].mean(),inplace=True)
+nan_data = initial_data[initial_data.CO2EMISSIONS.isna()]
+print(nan_data.to_string())
+print(len(initial_data.index)-initial_data.count())
