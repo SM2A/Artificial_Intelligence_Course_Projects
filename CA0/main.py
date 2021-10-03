b36802acff8c18693d89c696944bfb79b4caaa99
SM2A <seyedamin1990@gmail.com>
2021-10-03 20:14:41 +0330

    Part 1 & 2

--- CA0/main.py
+++ CA0/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+initial_data = pandas.read_csv('CA0\\FuelConsumptionCo2.csv')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print(initial_data.head)
+# print(initial_data.tail)
+print(initial_data.info())
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Artificial Intelligence Course Projects
 This repository contains my Artificial Intelligence Course Projects (Fall 2021) at University of Tehran.
 
+1. CA0 (Warmup) : Introduction to pandas, numpy, and matplotlib libraries
+
