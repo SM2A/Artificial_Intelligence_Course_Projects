f637770bed2a607edea4ea7e6adda4fe71c0d752
SM2A <seyedamin1990@gmail.com>
2021-10-04 15:24:10 +0330

    Part 5

--- CA0/main.py
+++ CA0/main.py
@@ -9,8 +9,23 @@ nan_data = initial_data[initial_data.CO2EMISSIONS.isna()]
 # print(nan_data.to_string())
 # print(len(initial_data.index)-initial_data.count())
 # print(initial_data.describe())
-print(initial_data['FUELCONSUMPTION_CITY'].mean())
+# print(initial_data['FUELCONSUMPTION_CITY'].mean())
 # print(initial_data[initial_data['FUELCONSUMPTION_CITY'] < 240].FUELCONSUMPTION_CITY.mean())
 # print(initial_data[initial_data['FUELCONSUMPTION_CITY'] < 240].FUELCONSUMPTION_CITY.mean())
-print(initial_data[initial_data['CO2EMISSIONS'] < 240].FUELCONSUMPTION_CITY.mean())
-print(initial_data[initial_data['CO2EMISSIONS'] > 300].FUELCONSUMPTION_CITY.mean())
+# print(initial_data[initial_data['CO2EMISSIONS'] < 240].FUELCONSUMPTION_CITY.mean())
+# print(initial_data[initial_data['CO2EMISSIONS'] > 300].FUELCONSUMPTION_CITY.mean())
+
+co2_240_sum = 0
+co2_240_count = 0
+co2_300_sum = 0
+co2_300_count = 0
+for index,row in initial_data.iterrows():
+    if row['CO2EMISSIONS'] < 240:
+        co2_240_sum += row['FUELCONSUMPTION_CITY']
+        co2_240_count += 1
+    elif row['CO2EMISSIONS'] > 300:
+        co2_300_sum += row['FUELCONSUMPTION_CITY']
+        co2_300_count += 1
+# start = time.time()
+print(f"CO2 Emotion < 240 : {co2_240_sum/co2_240_sum}")
+print(f"CO2 Emotion > 300 : {co2_300_sum/co2_300_count}")
