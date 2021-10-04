5dd030ad841ada0ee69fbca51ea9f1610494f12a
SM2A <seyedamin1990@gmail.com>
2021-10-04 16:20:58 +0330

    Part 6

--- CA0/main.py
+++ CA0/main.py
@@ -1,4 +1,5 @@
 import pandas
+from matplotlib import pyplot
 
 initial_data = pandas.read_csv('CA0\\FuelConsumptionCo2.csv')
 
@@ -15,17 +16,62 @@ nan_data = initial_data[initial_data.CO2EMISSIONS.isna()]
 # print(initial_data[initial_data['CO2EMISSIONS'] < 240].FUELCONSUMPTION_CITY.mean())
 # print(initial_data[initial_data['CO2EMISSIONS'] > 300].FUELCONSUMPTION_CITY.mean())
 
-co2_240_sum = 0
-co2_240_count = 0
-co2_300_sum = 0
-co2_300_count = 0
-for index,row in initial_data.iterrows():
-    if row['CO2EMISSIONS'] < 240:
-        co2_240_sum += row['FUELCONSUMPTION_CITY']
-        co2_240_count += 1
-    elif row['CO2EMISSIONS'] > 300:
-        co2_300_sum += row['FUELCONSUMPTION_CITY']
-        co2_300_count += 1
-# start = time.time()
-print(f"CO2 Emotion < 240 : {co2_240_sum/co2_240_sum}")
-print(f"CO2 Emotion > 300 : {co2_300_sum/co2_300_count}")
+# co2_240_sum = 0
+# co2_240_count = 0
+# co2_300_sum = 0
+# co2_300_count = 0
+# for index,row in initial_data.iterrows():
+#     if row['CO2EMISSIONS'] < 240:
+#         co2_240_sum += row['FUELCONSUMPTION_CITY']
+#         co2_240_count += 1
+#     elif row['CO2EMISSIONS'] > 300:
+#         co2_300_sum += row['FUELCONSUMPTION_CITY']
+#         co2_300_count += 1
+# # start = time.time()
+# print(f"CO2 Emotion < 240 : {co2_240_sum/co2_240_sum}")
+# print(f"CO2 Emotion > 300 : {co2_300_sum/co2_300_count}")
+
+initial_data['FUELCONSUMPTION_CITY'].hist(grid=True,legend=True)
+# pyplot.subplot(1)
+# initial_data['ENGINESIZE'].hist(grid=True)
+# pyplot.suptitle("Fuel Consumption City Histogram")
+# pyplot.ylabel("Frequency")
+# pyplot.xlabel("Fuel Consumption")
+
+# pyplot.subplot(2)
+# initial_data['CYLINDERS'].hist(grid=True)
+# pyplot.suptitle("Fuel Consumption City Histogram")
+# pyplot.ylabel("Frequency")
+# pyplot.xlabel("Fuel Consumption")
+
+# pyplot.subplot()
+# initial_data['FUELCONSUMPTION_CITY'].hist(grid=True)
+# pyplot.suptitle("Fuel Consumption City Histogram")
+# pyplot.ylabel("Frequency")
+# pyplot.xlabel("Fuel Consumption")
+
+# pyplot.subplot()
+# initial_data['FUELCONSUMPTION_HWY'].hist(grid=True)
+# pyplot.suptitle("Fuel Consumption City Histogram")
+# pyplot.ylabel("Frequency")
+# pyplot.xlabel("Fuel Consumption")
+
+# pyplot.subplot()
+# initial_data['FUELCONSUMPTION_COMB'].hist(grid=True)
+# pyplot.suptitle("Fuel Consumption City Histogram")
+# pyplot.ylabel("Frequency")
+# pyplot.xlabel("Fuel Consumption")
+
+# pyplot.subplot()
+# initial_data['FUELCONSUMPTION_COMB_MPG'].hist(grid=True)
+# pyplot.suptitle("Fuel Consumption City Histogram")
+# pyplot.ylabel("Frequency")
+# pyplot.xlabel("Fuel Consumption")
+
+# pyplot.subplot()
+# initial_data['CO2EMISSIONS'].hist(grid=True)
+# pyplot.suptitle("Fuel Consumption City Histogram")
+# pyplot.ylabel("Frequency")
+# pyplot.xlabel("Fuel Consumption")
+
+
