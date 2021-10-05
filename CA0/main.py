6336936a302024762a35b75ee773a283df0337aa
SM2A <seyedamin1990@gmail.com>
2021-10-05 14:13:33 +0330

    Modification

--- CA0/main.py
+++ CA0/main.py
@@ -1,5 +1,6 @@
 import pandas
 from matplotlib import pyplot
+import numpy
 
 initial_data = pandas.read_csv('CA0\\FuelConsumptionCo2.csv')
 
@@ -7,6 +8,7 @@ initial_data['FUELTYPE'] = initial_data['FUELTYPE'].astype('category').cat.codes
 initial_data['ENGINESIZE'].fillna(value=initial_data['ENGINESIZE'].mean(), inplace=True)
 initial_data['CYLINDERS'].fillna(value=initial_data['CYLINDERS'].mean(), inplace=True)
 nan_data = initial_data[initial_data.CO2EMISSIONS.isna()]
+initial_data.dropna(subset=['CO2EMISSIONS'], inplace=True)
 # print(nan_data.to_string())
 # print(len(initial_data.index)-initial_data.count())
 # print(initial_data.describe())
@@ -123,7 +125,52 @@ normalized_data['CO2EMISSIONS'] = (initial_data['CO2EMISSIONS'] - initial_data['
 # pyplot.ylabel('CO2EMISSIONS')
 # pyplot.show()
 
-data = [initial_data['FUELCONSUMPTION_COMB'], initial_data['FUELCONSUMPTION_COMB']]
-header = ['FUELCONSUMPTION_COMB', 'FUELCONSUMPTION_COMB']
+data = [initial_data['FUELCONSUMPTION_CITY'], initial_data['CO2EMISSIONS']]
+header = ['FUELCONSUMPTION_CITY', 'CO2EMISSIONS']
 linear_data = pandas.concat(data, axis=1, keys=header)
-print(linear_data.to_string())
+# print(linear_data.to_string())
+
+# from sklearn.linear_model import LinearRegression
+#
+# print(linear_data.values)
+# x_train = linear_data['FUELCONSUMPTION_CITY'].values
+# x_train = x_train.reshape(len(x_train),1)
+# y_train = linear_data['CO2EMISSIONS'].values
+#
+# lr = LinearRegression()
+#
+# lr.fit(x_train, y_train)
+# LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+#
+# nan_data['CO2EMISSIONS'] = lr.predict(nan_data['FUELCONSUMPTION_CITY'].values.reshape((-1,1)))
+#
+# print(nan_data.to_string())
+
+# pyplot.scatter(linear_data['FUELCONSUMPTION_CITY'],linear_data['CO2EMISSIONS'])
+# print(x_train)
+# print("#########################################################################################################")
+# print(y_train)
+# b = numpy.linalg.inv(x_train).dot(y_train)
+# print(b)
+
+X, y = linear_data.values[:,0], linear_data.values[:,1]
+X = X.reshape((len(X), 1))
+# calculate coefficients
+b = numpy.linalg.pinv(X).dot(y)
+print(b)
+# predict using coefficients
+yhat = X.dot(b)
+# plot data and predictions
+pyplot.scatter(X, y)
+pyplot.plot(X, yhat, color='red')
+pyplot.show()
+m = (X[0]-X[1])/(yhat[0]-yhat[1])
+print(m)
+print(y[0]-(m*X[0]))
+
+sum = 0
+i = 0
+for index,row in linear_data.iterrows():
+    sum += (row['CO2EMISSIONS']-yhat[i]) ** 2
+    i += 1
+print(sum/i)
