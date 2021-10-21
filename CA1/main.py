de062ef2367275f1306bf4f0cf748cf24e394d15
SM2A <seyedamin1990@gmail.com>
2021-10-21 20:10:43 +0330

    BFS

--- CA1/main.py
+++ CA1/main.py
@@ -13,51 +13,55 @@ class Cell(enum.Enum):
 
 
 temple = []
+m, n = 0, 0
 
 
-class doctor:
-    def __init__(self, id, x, y):
-        self.id = id
-        self.path = [(x, y)]
-
-    def get_path(self):
-        return self.path
-
-    def get_position(self):
-        return self.path[-1]
-
-    def add_dest(self, x, y):
-        self.path[-1][0] += x
-        self.path[-1][1] += y
-
-    def __hash__(self):
-        return f"{self.id} : {self.path}"
-
-    def __eq__(self, other):
-        return self.__hash__() == other.__hash__()
+# class doctor:
+#     def __init__(self, id, x, y):
+#         self.id = id
+#         self.position = (x, y)
+#
+#     def get_position(self):
+#         return self.position
+#
+#     def __str__(self):
+#         return f"{self.id} : {self.position}"
+#
+#     def __hash__(self):
+#         return f"{self.id} : {self.position}".__hash__()
+#
+#     def __eq__(self, other):
+#         return self.__hash__() == other.__hash__()
 
 
 class state:
-    def __init__(self, doubles, potions, doctors, x, y):
+    def __init__(self, doubles, potions, doctors, parent):
         self.doubles = doubles
         self.potions = potions
         self.doctors = doctors
-        self.x = x
-        self.y = y
+        self.parent = parent
 
-    def get_path(self):
-        path = []
-        for doc in self.doctors:
-            for pos in doc:
-                path.append(pos)
-        return path
+    def print_path(self):
+        result = "Drugs : "
+        result += str(self.doubles)
+        result += " | Potions "
+        result += str(self.potions)
+        result += "\nDoctors : "
+        current = self
+        while current is not None:
+            result += "\n"
+            for i in range(len(current.doctors)):
+                result += f"{i} : {current.doctors[i]} "
+            result += f" # STR : {current.__str__()} # HASH : {current.__hash__()}"
+            current = current.parent
+        return result
 
     def is_done(self):
         done = True
         if len(self.potions) != 0:
             return False
         for doc in self.doctors:
-            if doc.path[-1][-1] != (x, y):
+            if doc != (int(n)-1, int(m)-1):
                 return False
         return done
 
@@ -67,19 +71,12 @@ class state:
         result += " "
         result += str(self.potions)
         result += " "
-        for doc in self.doctors:
-            result += (doc.__hash__() + " ")
+        for i in range(len(self.doctors)):
+            result += f"{i} : {self.doctors[i]} "
         return result
 
     def __hash__(self):
-        result = ""
-        result += str(self.doubles)
-        result += " "
-        result += str(self.potions)
-        result += " "
-        for doc in self.doctors:
-            result += (doc.__hash__() + " ")
-        return result.__hash__()
+        return self.__str__().__hash__()
 
     def __eq__(self, other):
         return self.__hash__() == other.__hash__()
@@ -93,38 +90,74 @@ def next_state(frontier, visited):
     for i in range(len(frontier)):
         for j in range(len(frontier[i].doctors)):
             for adj in move:
-                new_s = copy.deepcopy(frontier[i])
-                pos = new_s.doctors[j].path[-1]
+                pos = frontier[i].doctors[j]
                 new_pos = (pos[0] + adj[0], pos[1] + adj[1])
-                new_s.doctors[j].path.append(new_pos)
-                if new_pos[0] >= int(new_s.y) or new_pos[0] < 0 or new_pos[1] >= int(new_s.x) or new_pos[1] < 0:
+
+                # if frontier[i].doctors[0] == (int(n) - 1, int(m) - 1) and len(frontier[i].doctors) == 2:
+                #     print(frontier[i].print_path())
+                # if len(frontier[i].doctors) == 2 and len(frontier[i].potions) == 0:
+                #     if frontier[i].doctors[-1] == (int(n) - 1, int(m) - 1) and frontier[i].doctors[-2] == (int(n) - 1, int(m) - 1):
+                #         print(f"{frontier[i].is_done()} $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+                #         print(frontier[i].print_path())
+                if pos == (int(n) - 1, int(m) - 1):
                     continue
-                elif temple[new_pos[0]][new_pos[1]] == Cell.wall:
+                if new_pos[0] >= int(n) or new_pos[0] < 0 or new_pos[1] >= int(m) or new_pos[1] < 0:
                     continue
-                if (j, pos, new_pos) not in visited:
+                if temple[new_pos[0]][new_pos[1]] == Cell.wall:
+                    continue
+
+                new_s = copy.deepcopy(frontier[i])
+                # dd = list(frontier[i].doubles)
+                # pp = list(frontier[i].potions)
+                # ddd = list(frontier[i].doctors)
+                # ddd[j] = new_pos
+                # new_s = state(dd, pp, ddd, frontier[i])
+                new_s.doctors[j] = new_pos
+                new_s.parent = frontier[i]
+                # strr = new_s.__hash__()
+
+                # is_in_v = False
+                # for kk in visited:
+                #     if  kk.doctors == new_s.doctors and kk.parent.doctors == new_s.parent.doctors:
+                #         is_in_v = True
+                #         break
+
+                if new_s not in visited:
                     ns.append(new_s)
-                    visited.add((j, pos, new_pos))
+                    # visited.append(new_s.__str__())
+                    # print(new_s.__hash__())
+                    # print(new_s.__str__())
+                    visited.add(new_s)
+                    # visited = sorted(visited)
+                # print(new_s not in visited)
+                # print(frontier[i] in visited)
     return ns
 
 
 def bfs(initial):
     visited = set()
     queue = [initial]
+    level = 0
     while queue:
         queue = next_state(queue, visited)
+        print(f"level {level} ----------------------------------------------------------------------------------------")
+        # if level == 14:
+        #     visited = sorted(visited)
+        #     print("ok")
+        #     print("asd")
+        # print(ssss)
         for f in queue:
-            print(f.__str__())
             for doc in f.doctors:
-                x = doc.path[-1][0]
-                y = doc.path[-1][1]
+                x, y = doc
                 if temple[x][y] == Cell.potion and (x, y) in f.potions:
                     f.potions.remove((x, y))
-                if temple[x][y] == Cell.double and (x, y) in f.doubles:
-                    new_doc = doctor(len(f.doctors) + 1, int(f.x) - 1, 0)
-                    f.doctors.append(new_doc)
+                elif temple[x][y] == Cell.double and (x, y) in f.doubles:
+                    f.doctors.append((int(n) - 1, 0))
                     f.doubles.remove((x, y))
                 if f.is_done():
                     return f
+            # print(f.print_path())
+        level += 1
 
 
 if __name__ == '__main__':
@@ -156,9 +189,17 @@ if __name__ == '__main__':
         x, y = file.readline().split()
         temple[int(x)][int(y)] = Cell.wall
 
-    doc = doctor(1, 0, 0)
-    initial_state = state(double, potion, [doc], int(n) - 1, int(m) - 1)
+    initial_state = state(double, potion, [(0, 0)], None)
+    # one = state(double, potion, [(0, 0)], None)
+    # two = state(double, potion, [(0, 0)], initial_state)
+    # three = state(double, potion, [(0, 0)], two)
+    # print(initial_state == one)
+    # print(initial_state == two)
+    # print(initial_state == three)
+    # print(two == one)
+    # print(three == one)
+    # print(three == two)
     begin = time.time()
     result = bfs(initial_state)
-    print(result.__str__())
+    print(result.print_path())
     print(f"Executed in {time.time() - begin} seconds")
